bd0cb823406d12257cfb86ffbcfaf112af1c7334
luizfk <luiz.kessler7@gmail.com>
2022-09-01 11:27:37 -0300

    array

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+// No primeiro exercício vamos praticar a criação de arrays. Crie 3 arrays diferentes:
+
+// - O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+const aNumero = [22, 15, 40, 75, 35]
+// - O segundo array deve possuir apenas **strings**;
+const aString = ["Lasanha", "Churrasco", "Pizza", "Pastel"]
+// - O terceiro array deve possuir **números**, **strings** e **booleanos**.
+const aMisto = [72, "Olá", 6 < 3 || 5 == 5]
+// Imprima cada um dos arrays acima, utilizando `console.log()`
+console.log(aNumero,
+aString,
+aMisto)
